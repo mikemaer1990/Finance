9410c5b618881dbe47ec09931d77349f62a6a2d2
mikemaer1990 <30853323+mikemaer1990@users.noreply.github.com>
2020-12-02 16:33:21 +0800

    enabled flask

--- application.py
+++ application.py
@@ -297,3 +297,7 @@ def errorhandler(e):
 # Listen for errors
 for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
