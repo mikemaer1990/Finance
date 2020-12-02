0df58169de58dc4a724bdd4d1369a559a302ca82
mikemaer1990 <30853323+mikemaer1990@users.noreply.github.com>
2020-12-02 17:08:59 +0800

    correct database path

--- application.py
+++ application.py
@@ -36,7 +36,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:////Coding/finance/finance/finance.db")
+db = SQL("sqlite:///finance.db")
 
 
 @app.route("/")
