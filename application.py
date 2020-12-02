b069026d12418eaa69ac4315416d351437bea584
mikemaer1990 <30853323+mikemaer1990@users.noreply.github.com>
2020-12-02 18:23:34 +0800

    ok

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from os import environ
 
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
-from flask_session import Session, redis
+from flask_session import Session
 from flask import Markup
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
